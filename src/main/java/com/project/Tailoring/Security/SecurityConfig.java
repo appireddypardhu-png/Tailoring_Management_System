30e0e8dd7ab787a6dc295887caf72ca74fcbccd5
Pardhu <appireddypardhu@gmail.com>
2026-05-30 13:43:40 +0530

    Register JWT filter

--- src/main/java/com/project/Tailoring/Security/SecurityConfig.java
+++ src/main/java/com/project/Tailoring/Security/SecurityConfig.java
@@ -1,52 +1,61 @@
 package com.project.Tailoring.Security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
-	@Autowired
-	private JwtFilter jwtFilter;
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
+    @Autowired
+    private JwtFilter jwtFilter;
 
-	    return new BCryptPasswordEncoder();
-	}
     @Bean
-	public SecurityFilterChain securityFilterChain(
-	        HttpSecurity http
-	) throws Exception {
-	
-	    http
-	        .cors(cors -> {})
-	        .csrf(csrf -> csrf.disable())
-	
-	        .sessionManagement(session ->
-	                session.sessionCreationPolicy(
-	                        SessionCreationPolicy.STATELESS
-	                )
-	        )
-	
-	        .authorizeHttpRequests(auth -> auth
-	                .requestMatchers("/auth/**").permitAll()
-	                .anyRequest().authenticated()
-	        )
-	
-	        .addFilterBefore(
-	                jwtFilter,
-	                UsernamePasswordAuthenticationFilter.class
-	        );
-	
-	    return http.build();
-	}
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+    ) throws Exception {
+
+        http
+
+            .cors(cors -> {})
+
+            .csrf(csrf -> csrf.disable())
+
+            .sessionManagement(session ->
+                    session.sessionCreationPolicy(
+                            SessionCreationPolicy.STATELESS
+                    )
+            )
+
+            .authorizeHttpRequests(auth -> auth
+
+                    .requestMatchers("/auth/**")
+                    .permitAll()
+
+                    .anyRequest()
+                    .authenticated()
+            )
+
+            .addFilterBefore(
+                    jwtFilter,
+                    UsernamePasswordAuthenticationFilter.class
+            );
+
+        return http.build();
+    }
 }
